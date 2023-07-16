ce306d73b7e636eadafd3ef2271b2f4bf826ff38
Luca Del Puppo <luca@delpuppo.net>
2023-07-16 11:33:14 +0200

    partial type

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-type Square = {
-  type: "square";
-  size: number;
+type Options = {
+  debug: boolean;
+  log: "info" | "warn" | "error";
+  port: number;
 };
 
-type Rectangle = {
-  type: "rectangle";
-  width: number;
-  height: number;
-};
-
-type Circle = {
-  type: "circle";
-  radius: number;
-};
-
-type Shape = Square | Rectangle | Circle;
-
-function area(shape: Shape): number {
-  switch (shape.type) {
-    case "square":
-      return shape.size * shape.size;
-    case "rectangle":
-      return shape.width * shape.height;
-    case "circle":
-      return Math.pow(shape.radius, 2) * Math.PI;
-  }
+function start({ debug = true, log = "warn", port = 3000 }: Partial<Options>) {
+  // do something
 }
+
+start({});
