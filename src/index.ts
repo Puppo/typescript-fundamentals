e62e47aec626ba4be056c5077c97b51f961a75f7
Luca Del Puppo <luca@delpuppo.net>
2023-07-16 12:16:47 +0200

    exclude type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-type Person = {
-  name: string,
-  age: number,
-  address: string
-  city: string,
-};
+type Status = "idle" | "loading" | "loaded" | "error";
 
-// type MyOmit<T, U extends keyof T> = {
-//   [K in Exclude<keyof T, U>]: T[K]
-// }
+type MyExclude<T, U> = T extends U ? never : T;
 
-type PersonWithoutAddress =
-  Omit<Person, 'address' | 'city'>
+type StatusWithoutError = Exclude<Status, "error">;
+
+const t: StatusWithoutError = "idle";
