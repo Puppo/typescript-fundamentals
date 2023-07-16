1237130c3ca1d3ea5527108c2df555fc0d8010b6
Luca Del Puppo <luca@delpuppo.net>
2023-07-16 11:06:15 +0200

    literals

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+type Color = "red" | "blue" | "green";
+
+const red = "red" satisfies Color;
+const green = "green" satisfies Color;
+const blue = "blue" satisfies Color;
+// const blue2 = "blue2" satisfies Color;
